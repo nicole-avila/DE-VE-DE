1780ae0544f2e321afc505d1790ba4baefa11d47
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2022-12-09 11:35:28 +0100

    start - html,css,js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBDo8IMD8fEuPkCJwDfmIa46AIOaxN5gf0",
+    authDomain: "slutexamination-wk.firebaseapp.com",
+    projectId: "slutexamination-wk",
+    storageBucket: "slutexamination-wk.appspot.com",
+    messagingSenderId: "741707634061",
+    appId: "1:741707634061:web:e5f91d7df6b361290f59c4"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app); 
+
+  
